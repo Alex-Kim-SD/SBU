9ef75a581c31a8408fed7d52bd1cb9b3abe70502
alex5 <alex@kimnetics.com>
2023-07-22 10:58:22 -1000

    styling getting nice

--- react-app/src/components/SideNavBar/SideNavBar.js
+++ react-app/src/components/SideNavBar/SideNavBar.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './SideNavBar.css';
 
 const SideNavBar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleDropdown = () => setIsOpen(!isOpen);
+
     return (
         <div className="side-nav">
-            <ul>
-                <li>
-                    <NavLink to="/Challenge">Challenge</NavLink>
+            <ul className="main-list">
+                <li className="main-list-item">
+                    <NavLink to="/Challenge">.challenge</NavLink>
                 </li>
-                <li>
-                    <NavLink exact to="/bots">Bots</NavLink>
+                <li className="main-list-item">
+                    <NavLink exact to="/debates">.debate_logs</NavLink>
                 </li>
-                <li>
-                    <NavLink exact to="/settings">Conv. Settings</NavLink>
-                </li>
-                <li>
-                    <NavLink exact to="/other-bots">Other Bots</NavLink>
-                </li>
-                <li>
-                    <NavLink exact to="/debates">Debates</NavLink>
+                <li className='dropdown-trigger' onClick={toggleDropdown}>
+                    <span className="main-list-item" >.config</span>
+                    <ul className={`dropdown-list ${isOpen ? 'show' : ''}`}>
+                        <li className="dropdown-item">
+                            <NavLink exact to="/bots">Bots-Primary</NavLink>
+                        </li>
+                        <li className="dropdown-item">
+                            <NavLink exact to="/settings">Conversations</NavLink>
+                        </li>
+                        <li className="dropdown-item">
+                            <NavLink exact to="/other-bots">Bots-Opponent</NavLink>
+                        </li>
+                    </ul>
                 </li>
             </ul>
         </div>
