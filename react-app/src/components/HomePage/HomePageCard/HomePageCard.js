4a18d68076acee84bb1f1509693a38c054f5d2c3
alex5 <alex@kimnetics.com>
2023-07-22 10:29:22 -1000

    css improvements

--- react-app/src/components/HomePage/HomePageCard/HomePageCard.js
+++ react-app/src/components/HomePage/HomePageCard/HomePageCard.js
@@ -6,7 +6,7 @@ function HomePageCard() {
     return (
         <div className="HomePageCard">
             <h3 className="HomePageCard-title"> --i System Initialized.</h3>
-            <p className="HomePageCard-p-tag">Hello World, and welcome to SBU Chat.</p>
+            <p className="HomePageCard-p-tag">Hello World, and <br /> welcome to SBU Chat.</p>
         </div>
     );
 }
