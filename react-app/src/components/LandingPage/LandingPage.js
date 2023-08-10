fb30dcc272d7cb06132f8adcac0a970b9712e26d
alex5 <alex@kimnetics.com>
2023-08-09 21:20:44 -1000

    demo login button

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -9,12 +9,14 @@ function LandingPage() {
 
   if (sessionUser) return <Redirect to="/home" />;
 
+
   return (
     <div className="landing-page-container">
       <div className="landing-page-header-box">
         <h1 className="landing-page-title">Welcome to SBU</h1>
         <p className="landing-page-subtitle">Generate AI Conversations, Stop Being Unreasonable</p>
         <div className="landing-page-buttons">
+          <div className="demo-login"> Demo-Login </div>
           <Link to="/login" className="landing-page-btn login-btn">Login</Link>
           <Link to="/signup" className="landing-page-btn signup-btn">Sign Up</Link>
         </div>
