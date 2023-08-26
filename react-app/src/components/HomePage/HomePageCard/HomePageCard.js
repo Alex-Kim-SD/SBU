bd80769fe0332af9a41390802ba2a5637eeca4a4
alex5 <alex@kimnetics.com>
2023-08-26 00:41:39 -1000

    description added

--- react-app/src/components/HomePage/HomePageCard/HomePageCard.js
+++ react-app/src/components/HomePage/HomePageCard/HomePageCard.js
@@ -7,6 +7,10 @@ function HomePageCard() {
         <div className="HomePageCard">
             <h3 className="HomePageCard-title"> --i System Initialized.</h3>
             <p className="HomePageCard-p-tag">Hello World, and <br /> welcome to SBU Chat.</p>
+            <p className="HomePageCard-p-tag">Let's generate a conversation!</p>
+            <p className="HomePageCard-p-small"> Step 1. Create a bot personality.</p>
+            <p className="HomePageCard-p-small"> Step 2. Describe the setting.</p>
+            <p className="HomePageCard-p-small"> Step 3. Generate the conversation!</p>
         </div>
     );
 }
